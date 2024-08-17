c68219c7b708401e9b527df6b492698fd9b4efeb
bryanathll <bryannatanael77@gmail.com>
2024-08-17 14:40:47 +0700

    feat: setup spatie

--- database/seeders/RoleTableSeeder.php
+++ database/seeders/RoleTableSeeder.php
@@ -13,7 +13,7 @@ class RoleTableSeeder extends Seeder
      */
     public function run(): void
     {
-        Role::create(['name' =>'admin']);
-        Role::create(['name' =>'user']);
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'user']);
     }
 }
