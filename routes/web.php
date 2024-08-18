50ee7d0b3f77cae56b0d491910e0f41250356ff3
bryanathll <bryannatanael77@gmail.com>
2024-08-18 16:39:14 +0700

    feat: init commit

--- routes/web.php
+++ routes/web.php
@@ -5,22 +5,13 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+Route::redirect('/', '/prototype/login');
+
+Route::prefix('prototype')->group(function(){
+    route::get('/login', function(){
+        return Inertia::render('Prototype/Login');
+    });
 });
-
-Route::get('admin', function(){
-    return 'Hi Admin';
-})->middleware('role:admin');
-
-Route::get('user', function(){
-    return 'Hi User';
-})->middleware('role:user');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
